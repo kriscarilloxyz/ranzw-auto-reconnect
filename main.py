cf3f5bd3ed34f91979265ff7ef2c6b9c7db0f383
kriscarilloxyz <kriscarilloxyz@gmail.com>
2020-10-16 01:25:30 +0800

    added empty funcs

---
+++ main.py
@@ -0,0 +1,38 @@
+import pyautogui
+
+def start_game():
+  pass
+
+def confirm_update():
+  pass
+
+def accept_startup():
+  pass
+
+def choose_server():
+  pass
+
+def choose_channel():
+  pass
+
+def fill_username():
+  pass
+
+def fill_password():
+  pass
+
+def click_login():
+  pass
+
+def click_character():
+  pass
+
+def click_accept_character():
+  pass
+
+def check_disconnect():
+  pass
+
+
+if __name__ == "__main__":
+    pass
